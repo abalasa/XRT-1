5641f82c06d3c478c8315c73fd1e7edca33df134
apurvadixit <apurvadixit@users.noreply.github.com>
2021-03-25 13:24:41 -0700

    test 3/25

--- src/CMake/ccache.cmake
+++ src/CMake/ccache.cmake
@@ -4,7 +4,7 @@ if (RDI_CCACHE)
   find_program(gccarwrap /tools/batonroot/rodin/devkits/lnx64/ccwrap/gccarwrap)
 
   if (gccwrap)
-    message ("-- Using compile cache wrapper ${gccwrap} with cache in $ENV{RDI_CCACHEROOT}")
+    message ("------- Using compile cache wrapper ${gccwrap} with cache in $ENV{RDI_CCACHEROOT}")
     set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE "${gccwrap} --with-cache-rw")
   endif ()
 
